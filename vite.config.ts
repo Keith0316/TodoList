09064ca1d57e1bf91fc0b8c4c74dad8a59521533
hp3306 <1554938485@qq.com>
2026-06-01 15:56:40 +0800

    fix: 配置别名路径，修复构建问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/TodoList/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
